4fd8a2e3b0b0999b7919e72d5cfbd10892ca56b7
Branko Blagojevic <branko.blagojevic@gmail.com>
2020-03-24 07:33:19 -0400

    sls dep

--- sls-search-compare/handler.ts
+++ sls-search-compare/handler.ts
@@ -1,23 +1,23 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import rp from 'request-promise'
-import { parseDdg, parseGoogleHtml } from './utils'
+import { APIGatewayProxyHandler } from "aws-lambda"
+import rp from "request-promise"
+import { parseDdg, parseGoogleHtml } from "./utils"
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
       input: event,
     }, null, 2),
   }
 }
 
-export const ddg: APIGatewayProxyHandler = async (event, _context) => {
-  const search = event.pathParameters['search']
+export const ddg: APIGatewayProxyHandler = async (event: { pathParameters: string }, _context: any) => {
+  const search = event.pathParameters.search
   const url = `https://duckduckgo.com/html/?q=${search}`
   const headers = {
-    Host: "duckduckgo.com",
-    "User-Agent":" Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+    "Host": "duckduckgo.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
   }
 
   const results = await rp(url, {headers, rejectUnauthorized: false}).promise().then(parseDdg)
@@ -25,15 +25,15 @@ export const ddg: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
-    body
+    body,
   }
 }
 
 // export const google: APIGatewayProxyHandler = async (event, _context) => {
-  
+
 //   const search = event.pathParameters['search']
 //   const numPages = 2
 //   const pages = [...Array(numPages).keys()]
@@ -44,7 +44,7 @@ export const ddg: APIGatewayProxyHandler = async (event, _context) => {
 //   }))
 
 //   const body = JSON.stringify(results.reduce((acc, val) => [...acc, ...val]))
- 
+
 //   return {
 //     statusCode: 200,
 //     headers: {
